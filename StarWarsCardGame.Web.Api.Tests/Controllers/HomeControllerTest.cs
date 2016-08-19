4ddfa1de3c9119c2137915d46a576fa3fb88af01
koskapan <koskapan@gmail.com>
2016-08-19 14:03:46 +0300

    Initial

---
+++ StarWarsCardGame.Web.Api.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StarWarsCardGame.Web.Api;
+using StarWarsCardGame.Web.Api.Controllers;
+
+namespace StarWarsCardGame.Web.Api.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Упорядочение
+            HomeController controller = new HomeController();
+
+            // Действие
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Утверждение
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ StarWarsCardGame.Web.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace StarWarsCardGame.Web.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
